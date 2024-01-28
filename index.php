361749be69aa06e66b1a8d23896f166028c2b4d0
Maksym-Bohun <maks.boggun@gmail.com>
2024-01-28 21:23:29 +0200

    used arithmetic operations and functions

--- index.php
+++ index.php
@@ -1,25 +1,85 @@
 <?php
+/*
+| Arithmetic Operators
+| Operator | Description    |
+| -------- | -------------- |
+| `+`      | Addition       |
+| `-`      | Subtraction    |
+| `*`      | Multiplication |
+| `/`      | Division       |
+| `%`      | Modulus        |
+*/
 
-$number1 = 5;
-$number2 = 10;
-$number3 = '20';
-$fruit = 'apple';
-$bool1=true;
-$bool2=false;
-$null = null;
 
-// Implicit Conversion
+$output = null;
 
-$result = $number1 + $number2;
-$result = $number1 + $number2 + $number3;
-$result = $number3 . $fruit;
-$result = $number1 + $bool1;
-$result = $number1 + $bool2;
-$result = $number1 + $null;
+$num1 = 20;
+$num2 = 10;
 
-$result = (int) $number3;
-$result = (int) $bool1;
-$result = (string) $number1;
+$output = "$num1 + $num2 = " . $num1 + $num2;
+$output = "$num1 - $num2 = " . $num1 - $num2;
+$output = "$num1 * $num2 = " . $num1 * $num2;
+$output = "$num1 / $num2 = " . $num1 / $num2;
 
-var_dump($result);
-echo ('<br/>');
+
+// Assignment operator
+$num3 = 10;
+$num3 += 20;
+
+$output = $num3;
+
+// Built-in PHP functions
+// rand()
+$output = rand();
+$output = getrandmax();
+$output = rand(1, 100);
+
+//round()
+$output = round(0.47, 1);
+
+// ceil()
+$output = ceil(4.3);
+
+// floor()
+$output = floor(4.3);
+
+// abs()
+$output = abs(-2.4);
+
+// max()
+$output = max(1,2,40,23,40.001);
+$output = max([1,2,40,23,40.001]);
+
+// min()
+$output = min([1,2,40,23,40.001]);
+
+$output = number_format(123456789.19234, 2, '.', ',')
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://cdn.tailwindcss.com"></script>
+  <title>PHP From Scratch</title>
+</head>
+
+<body class="bg-gray-100">
+  <header class="bg-blue-500 text-white p-4">
+    <div class="container mx-auto">
+      <h1 class="text-3xl font-semibold">PHP From Scratch</h1>
+    </div>
+  </header>
+  <div class="container mx-auto p-4 mt-4">
+    <div class="bg-white rounded-lg shadow-md p-6 mt-6">
+      <!-- Output -->
+      <p class="text-xl">
+        <?= $output ?>
+      </p>
+    </div>
+  </div>
+</body>
+
+</html>
