ba47eedb6e87b38b1b1f976f2d865fc304cffc8a
Maksym-Bohun <maks.boggun@gmail.com>
2024-01-28 12:36:22 +0200

    used php tags, printing, comments and variables

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+    // tags, printing, comments
+
+    /*
+    echo 'Hello From PHP';
+    print '<br/>';
+    print 'Hello from print';
+    echo '<br/>','Value 1  ', 'Value 2';
+    */
+
+    // Variables
+
+    $title = 'Learn PHP from Scratch';
+    $heading = 'Welcome to the course!';
+    $body = 'In this course, you will learn the fundamentals of the PHP language';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title><?=  $title?>
+</title>
+</head>
+
+<body class="bg-gray-100">
+    <header class="bg-blue-500 text-white p-4">
+        <div class="container mx-auto">
+            <h1 class="text-3xl font-semibold"><?= $title ?></h1>
+        </div>
+    </header>
+    <div class="container mx-auto p-4 mt-4">
+        <div class="bg-white rounded-lg shadow-md p-6">
+            <h2 class="text-2xl font-semibold mb-4"><?= $heading ?></</h2>
+          <p><?= $body ?></p>
+        </div>
+    </div>
+</body>
+
+</html>
