efa85a3079a725455cd31692effa81f7ce19bdff
Maksym-Bohun <maks.boggun@gmail.com>
2024-01-29 21:27:27 +0200

    used null coalescing operator

--- index.php
+++ index.php
@@ -1,67 +1,13 @@
 <?php 
 
-$dayOfWeek = date('l');
-echo $dayOfWeek;
+// $favoriteColor = 'red';
 
-switch($dayOfWeek){
-    case 'Monday': 
-        $message = 'Monday blues';
-        $color = 'blue';
-        break;
-    case 'Tuesday': 
-        $message = 'At least it\'s not Monday';
-        $color = 'green';
-        break;
-        case 'Wednesday': 
-            $message = 'Hump day!';
-            $color = 'orange';
-            break;
-    case 'Thursday': 
-        $message = 'One more day until friday';
-        $color = 'red';
-        break;
-    case 'Friday': 
-        $message = 'TGIF!';
-        $color = 'purple';
-        break;
-    case 'Saturday': 
-        $message = 'Have a nice weekend!';
-        $color = 'magenta';
-        break;
-    case 'Sunday': 
-        $message = 'Have a nice weekend!';
-        $color = 'magenta';
-        break;
+// $color = isset($favoriteColor) ? $favoriteColor: 'blue';
 
-}
+// $color = $favoriteColor ?? 'blue';
 
-?>
+$color = isset($favoriteColor) ? $favoriteColor : (isset($secondFavoriteColor)? $secondFavoriteColor : 'blue' );
 
-<!DOCTYPE html>
-<html lang="en">
+$color = isset($favoriteColor) ?? $secondFavoriteColor ?? 'blue';
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>What Day Is It?</title>
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
-  <style>
-    body {
-      font-family: 'Roboto', sans-serif;
-      background-color: <?= $color ?>;
-      color: white;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      height: 100vh;
-    }
-  </style>
-</head>
-
-<body>
-  <h1><?= $text ?></h1>
-</body>
-
-</html>
+echo $color;
