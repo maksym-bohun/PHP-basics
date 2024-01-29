f4ecfe9bef658640d3653bb4e5bbb29b94fbd22c
Maksym-Bohun <maks.boggun@gmail.com>
2024-01-29 21:32:02 +0200

    completed coding challenge

--- index.php
+++ index.php
@@ -1,13 +1,19 @@
-<?php 
+<?php
 
-// $favoriteColor = 'red';
+/*
+Names Challenge:
+1. Take the array of names below and loop through them. The type of loop is up to you.
+2. Within the loop, use a conditional statement to check if the first letter of the name is 'A'.
+3. If the first letter is 'A', skip that name and continue to the next iteration.
+4. If the first letter is not 'A', reverse the string.
+5. Make all names lowercase before printing them.
+*/
 
-// $color = isset($favoriteColor) ? $favoriteColor: 'blue';
+$names = ['Alex', 'Beth', 'Caroline', 'Dave', 'Elanor', 'Anna', 'Freddie', 'Adam'];
 
-// $color = $favoriteColor ?? 'blue';
-
-$color = isset($favoriteColor) ? $favoriteColor : (isset($secondFavoriteColor)? $secondFavoriteColor : 'blue' );
-
-$color = isset($favoriteColor) ?? $secondFavoriteColor ?? 'blue';
-
-echo $color;
+foreach($names as $name){
+    if($name[0] === 'A') {continue;}
+    else{$name = strrev($name);}
+    $name = strtolower( $name );
+    echo $name . '<br>';
+}
