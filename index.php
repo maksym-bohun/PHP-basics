73aee3f7cc81f708166205a5699ebbad8920bb13
Maksym-Bohun <maks.boggun@gmail.com>
2024-01-29 10:04:01 +0200

    used array functions

--- index.php
+++ index.php
@@ -1,30 +1,92 @@
 <?php
+$output = null;
 
-$names = array('John', 'Jack', 'Jill');
-$numbers = [1,2,3,4,5,6,];
+$ids = [10, 22, 12, 43, 56];
+$users = ['user2', 'user1', 'user3'];
 
-function inspect($value){
-    echo '<pre>';
-    var_dump($value);
-    echo '<pre>';
-}
+$output = count($ids);
 
-// inspect($names);
-// inspect($numbers);
+// sort
+sort($ids); 
+sort($users); 
 
-// print_r($names);
+// rsort
+rsort($ids);
 
-// echo $names[0];
-// echo $numbers[4];
+// array_push
+array_push($ids,500);
+array_push($users,'user4');
 
-// Add element to array
-$numbers[6] = 100;
-$numbers[] = 1001; // push to the array
+// array_pop
+array_pop($ids);
+array_pop($users);
 
-$numbers[3] = 200;
+// array_shift
+array_shift($ids);
 
-unset($numbers[3]);
+// array_unshift
+array_unshift($users, 'user5');
 
-$numbers = array_values($numbers);
-inspect($numbers);
+// array_slice
+$ids2 = array_slice($ids, 2, 2);
+// var_dump($ids2)
 
+// array_splice
+// array_splice($ids, 1, 1, 'New id');
+
+// array_sum
+    $output = 'Sum of IDs: '. array_sum($ids);
+
+// array_search
+$output = 'User 2 is at index: ' . array_search('user2', $users);
+
+// explode
+$tags = 'tech, code, js, py, node.js';
+$tagsArr = explode(', ', $tags);
+// var_dump($tagsArr);
+
+//implode
+$output = implode(', ', $users);
+
+// in_array
+// $output = 'User 3 exists: ' . in_array('user3', $users);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>PHP From Scratch</title>
+</head>
+
+<body class="bg-gray-100">
+    <header class="bg-blue-500 text-white p-4">
+        <div class="container mx-auto">
+            <h1 class="text-3xl font-semibold">PHP From Scratch</h1>
+        </div>
+    </header>
+    <div class="container mx-auto p-4 mt-4">
+        <div class="bg-white rounded-lg shadow-md p-6 mt-6">
+            <!-- Output -->
+            <p class="text-xl"><?= $output ?></p>
+            <h2 class="text-xl font-semibold my-4">Ids array:</h2>
+            <p>
+                <pre>
+                    <?php print_r($ids) ?>
+                </pre>
+            </p>
+            <h2 class="text-xl font-semibold my-4">Users array:</h2>
+            <p>
+                <pre>
+                    <?php print_r($users) ?>
+                </pre>
+            </p>
+        </div>
+    </div>
+</body>
+
+</html>
