ab96e6c462228b0f930a1f0efe57cff552621c3f
Maksym-Bohun <maks.boggun@gmail.com>
2024-01-30 19:51:10 +0200

    used static methods and members

--- index.php
+++ index.php
@@ -1,44 +1,11 @@
 <?php
+class MathUtility{
+    public static $pi = 3.14;
 
-class User
-{
-  public $name;
-  public $email;
-  protected $status = 'active';
-
-  public function __construct($name, $email)
-  {
-    $this->name = $name;
-    $this->email = $email;
-  }
-
-  public function login()
-  {
-    echo $this->name . ' logged in <br>';
-  }
-}
-
-
-class Admin extends User{
-    public $level;
-
-    public function __construct($name, $email, $level)
-    {
-        $this->level = $level;
-        parent::__construct($name, $email);
-    }
-
-    public function getStatus(){
-        echo '</br>Status: ' . $this->status . '</br>';
-    }
-
-    public function login()
-    {
-      echo "Admin ".$this->name . ' logged in <br>';
+    public static function add(...$nums){
+        return array_sum($nums);
     }
 }
 
-$admin1 = new Admin('Tom Smith', 'tom@gmail.com', 5);
-
-echo $admin1->getStatus();
-$admin1->login();
+echo MathUtility::$pi;
+echo MathUtility::add(1, 2, 33);
